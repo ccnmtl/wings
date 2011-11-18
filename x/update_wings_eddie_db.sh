3adc26e5e2dddb5f07265e6a2c898d3cbc1c7a1b
Eddie Rubeiz <eddie@ccnmtl.columbia.edu>
2011-11-18 11:32:47 -0500

    small css change

--- x/update_wings_eddie_db.sh
+++ x/update_wings_eddie_db.sh
@@ -4,5 +4,5 @@ dropdb wings
 createdb -O eddie  wings
 psql eddie -d wings -f ./wings.sql
 psql eddie -d wings -c "update pagetree_hierarchy set name = 'tiur.ccnmtl.columbia.edu:12345';"
-psql eddie -d wings -c "update main_stand set hostname = 'tiur.ccnmtl.columbia.edu:12345' where id= 8;"
+psql eddie -d wings -c "update main_stand set hostname = 'tiur.ccnmtl.columbia.edu:12345' where title='wings';"
 
