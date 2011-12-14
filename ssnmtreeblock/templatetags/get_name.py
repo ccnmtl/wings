64bd1b1698e5f31619ee58529bf212258357480c
Eddie Rubeiz <eddie@ccnmtl.columbia.edu>
2011-12-14 14:56:19 -0500

    ok checkboxes update

--- ssnmtreeblock/templatetags/get_name.py
+++ ssnmtreeblock/templatetags/get_name.py
@@ -1,7 +1,6 @@
 from django import template
-from ssnmtreeblock.models import name_for_box
 register = template.Library()
     
 @register.simple_tag
 def get_name_for_box (box, user):
-    return name_for_box(box, user)
+    return box.name(user)
