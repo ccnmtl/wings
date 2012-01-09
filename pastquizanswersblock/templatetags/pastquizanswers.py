d8c775b5791dc72256ed7e36975c6ecd79137f7b
Eddie <eddie@tiboudchou.(none)>
2012-01-09 09:19:43 -0500

    pretty close to done

--- pastquizanswersblock/templatetags/pastquizanswers.py
+++ pastquizanswersblock/templatetags/pastquizanswers.py
@@ -14,82 +14,32 @@ def to_text (response):
         return response.value
     if response.question.is_long_text():
         return response.value
-
-
-    if response.question.is_single_choice():
-        return dict((r.value, r.label) for r in response.question.answer_set.all())[response.value]
-
     if response.question.is_multiple_choice():
-        assert 1 == 0 # not implementing this in Wings.
+	return "Multiple choice question: not implemented."
+    if response.question.is_single_choice() or  response.question.is_single_choice_dropdown():
+	answer_set = response.question.answer_set.all()
+	if answer_set:
+	    return dict((r.value, r.label) for r in answer_set)[response.value]
+	else:
+	    return 'No answer'
 
 @register.simple_tag
 def interpolate_previous_answers(user, block):
-    print user
-    print block.copy
-    #import pdb
-    #pdb.set_trace()
-    
-    
-    
+
     def find_the_response (matches):
+        """For each match, look up the question and return a text version of the answer to that question. TODO: make "no answer" error message an argument to this method."""
         que = Question.objects.get (id=matches.groups()[0])
-        u = user
         quiz = que.quiz
-        sub = Submission.objects.filter(quiz=quiz,user=u).order_by("-submitted")
+        sub = Submission.objects.filter(quiz=quiz,user=user).order_by("-submitted")
         if sub.count() == 0:
-            return ''
+            return 'No answer'
         submission = sub[0]
         res = Response.objects.filter(question=que,submission=submission)
         if res.count() > 0:
-            #import pdb
-            #pdb.set_trace()
             return to_text(res[0])
         else:
-            return ''
-            
-            
-            
-    
-    return re.sub("\[\[\[(\d+)\]\]\]", find_the_response, block.copy)
-    
-    
-    
-def narrow_down(context):
-    user = context['user']
-    question = Question.objects.get (id=context['question_id'])
-    nda, created = NarrowedDownAnswer.objects.get_or_create(user=user, question=question )
-    nda.save()
-    responses = question.user_responses (context['user'])
-    pageblock_id = context['pageblock_id']
-    return {
-        'checked_responses'    : [q.value for q in responses],
-        'question'             : question,
-        'responses'            : responses,
-        'pageblock_id'         : pageblock_id,
-        'narrowed_down_answer' : nda,
-    }
-register.inclusion_tag('servicesblock/narrow_down.html', takes_context=True)(narrow_down)
+            return 'No answer'
 
-
-
-def show_narrowed_down_answer(context):
-    question = Question.objects.get (id=context['question_id'])
-    nda, created = NarrowedDownAnswer.objects.get_or_create(user=context['user'], question=question )
-    nda.save()
-    return { 'narrowed_down_answer' : nda }
-register.inclusion_tag('servicesblock/show_narrowed_down_answer.html', takes_context=True)(show_narrowed_down_answer)
-
-
-
-def show_related_resources(context):
-    question = Question.objects.get (id=context['question_id'])
-    nda, created = NarrowedDownAnswer.objects.get_or_create(user=context['user'], question=question )
-    nda.save()
-    return { 
-        'resources' : ServiceProvider.objects.filter(issue = nda.answer)
-    }
-register.inclusion_tag('servicesblock/show_related_resources.html', takes_context=True)(show_related_resources)
-
-
-
+    #substitute triple-bracketed integers with the response to the question with the interger's ID.
+    return re.sub("\[\[\[(\d+)\]\]\]", find_the_response, block.copy)    
 
