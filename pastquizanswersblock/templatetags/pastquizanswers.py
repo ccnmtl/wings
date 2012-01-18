3653b4e9f4c1dea96e231e19b9297d5576fdacc6
Eddie Rubeiz <eddie@ccnmtl.columbia.edu>
2012-01-18 12:49:20 -0500

    refactoring pastquizanswersblock

--- pastquizanswersblock/templatetags/pastquizanswers.py
+++ pastquizanswersblock/templatetags/pastquizanswers.py
@@ -6,37 +6,72 @@ import re
 register = template.Library()
 
 
-def to_text (response):
-    if response.question.is_short_text():
-        return response.value
-    if response.question.is_long_text():
-        return response.value
+
+
+def print_if_testing (a):
+    if True:
+        print a
+
+def match_against_string (response, string_to_match):
+    if response.question.is_short_text() or response.question.is_long_text():
+        return response.value == string_to_match
     if response.question.is_multiple_choice():
-	return "Multiple choice question: not implemented."
+	    return False
     if response.question.is_single_choice() or  response.question.is_single_choice_dropdown():
-	answer_set = response.question.answer_set.all()
-	if answer_set:		
-	    return dict((r.value, r.label) for r in answer_set)[response.value]
-	else:
-	    return 'No answer'
+	    answer_set = response.question.answer_set.all()
+	    if not answer_set:
+	        return False
+	    tmp = dict((r.value, r.label) for r in answer_set)[response.value]        
+	    print_if_testing ('found an answer to ' + response.__unicode__())
+	    print_if_testing (tmp)
+	    return tmp == string_to_match
+
+
+def to_text (response):
+    if response.question.is_multiple_choice():
+	    return ''
+    if response.question.is_short_text() or response.question.is_long_text():
+        return response.value
+    if response.question.is_single_choice() or  response.question.is_single_choice_dropdown():
+	    answer_set = response.question.answer_set.all()
+	    if not answer_set:
+	        return ''
+	    tmp = dict((r.value, r.label) for r in answer_set)[response.value]   
+	    print_if_testing ('found an answer to ' + response.__unicode__())
+	    print_if_testing (tmp)  
+	    return tmp
 
 @register.simple_tag
-def interpolate_previous_answers(user, block):
-
-    def find_the_response (matches):
-        """For each match, look up the question and return a text version of the answer to that question. TODO: make "no answer" error message an argument to this method."""
-        que = Question.objects.get (id=matches.groups()[0])
-        quiz = que.quiz
-        sub = Submission.objects.filter(quiz=quiz,user=user).order_by("-submitted")
-        if sub.count() == 0:
-            return 'No answer'
-        submission = sub[0]
-        res = Response.objects.filter(question=que,submission=submission)
-        if res.count() > 0:
-            return escape(to_text(res[0]))
-        else:
-            return 'No answer'
-
-    #substitute triple-bracketed integers with the response to the question with the interger's ID.
-    return re.sub("\[\[\[(\d+)\]\]\]", find_the_response, block.copy)    
-
+def interpolate_previous_answer(user, question_id):
+    que = Question.objects.get (id=question_id)
+    quiz = que.quiz
+    sub = Submission.objects.filter(quiz=quiz,user=user).order_by("-submitted")
+    if sub.count() == 0:
+        return ''
+    submission = sub[0]
+    res = Response.objects.filter(question=que,submission=submission)
+    if res.count() > 0:
+        return escape(to_text(res[0]))
+    else:
+        return ''
+    
+    
+    
+@register.simple_tag
+def test_answer (user, question_id, value_to_match, output_if_match, output_if_no_match):
+    print_if_testing ('testing %d' % question_id)
+    
+    que = Question.objects.get (id=question_id)
+    if not que:
+        return "No question found with id %s" % question_id
+    
+    quiz = que.quiz
+    sub = Submission.objects.filter(quiz=quiz,user=user).order_by("-submitted")
+    if sub.count() == 0:
+        return output_if_no_match
+    submission = sub[0]    
+    res = Response.objects.filter(question=que,submission=submission)
+    if res.count() > 0 and match_against_string (res[0], value_to_match):
+        return output_if_match
+    return output_if_no_match
+    
