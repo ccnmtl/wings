71acc62547e5ea657ac466f477c32e89dd0087c4
eddie <eddie@kang.ccnmtl.columbia.edu>
2012-01-19 11:06:08 -0500

    second round

--- x/update_common_sandbox_and_kang_files.sh
+++ x/update_common_sandbox_and_kang_files.sh
@@ -1,2 +1,2 @@
-sudo -u pusher /home/pusher/update_wings_common_db.sh 
+sudo -u pusher ./update_wings_common_db.sh 
 ./copy_down_wings_files.sh
--- x/update_lizday_sandbox_and_kang_files.sh
+++ x/update_lizday_sandbox_and_kang_files.sh
@@ -1,2 +1,2 @@
-sudo -u pusher /home/pusher/update_wings_lizday_db.sh 
+sudo -u pusher ./update_wings_lizday_db.sh 
 ./copy_down_wings_files.sh
