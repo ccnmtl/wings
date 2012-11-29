a3a546a7714403acac28dedab4503ef52e5737c0
Eddie Rubeiz <eddie@ccnmtl.columbia.edu>
2012-11-29 13:28:28 -0500

    adding audio + help block rudimentary admin.py

---
+++ audioblock/admin.py
@@ -0,0 +1,8 @@
+from models import AudioBlock
+from django.contrib import admin
+
+class AudioBLockAdmin (admin.ModelAdmin):
+    list_display =  (  'audio_file',)
+    fields       =  (  'audio_file',)
+admin.site.register(AudioBlock, AudioBLockAdmin)
+
---
+++ helpblock/admin.py
@@ -0,0 +1,8 @@
+from models import HelpBlock
+from django.contrib import admin
+
+
+class HelpBLockAdmin (admin.ModelAdmin):
+    list_display =  (  'audio_file',)
+    fields       =  (  'audio_file',)
+admin.site.register(HelpBlock, HelpBLockAdmin)
