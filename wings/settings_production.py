a6ee928a8b5306d4ed09ceb7f74203098182d4d9
Anders Pearson <anders@columbia.edu>
2015-12-10 13:48:46 +0000

    convert to ccnmtlsettings

--- wings/settings_production.py
+++ wings/settings_production.py
@@ -1,36 +1,15 @@
 # flake8: noqa
 from settings_shared import *
+from ccnmtlsettings.production import common
 
-TEMPLATE_DIRS = (
-    "/var/www/wings/wings/wings/templates",
-)
-
-MEDIA_ROOT = '/var/www/wings/uploads/'
-# put any static media here to override app served static media
-STATICMEDIA_MOUNTS = (
-    ('/sitemedia', '/var/www/wings/wings/sitemedia'),
-)
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'wings',
-        'HOST': '',
-        'PORT': 6432,
-        'USER': '',
-        'PASSWORD': '',
-    }
-}
-
-COMPRESS_ROOT = "/var/www/wings/wings/media/"
-DEBUG = False
-TEMPLATE_DEBUG = DEBUG
-
-STATIC_ROOT = os.path.join(os.path.dirname(__file__), "../media")
-STATICFILES_DIRS = ()
-
-if 'migrate' not in sys.argv:
-    INSTALLED_APPS.append('raven.contrib.django.raven_compat')
+locals().update(
+    common(
+        project=project,
+        base=base,
+        STATIC_ROOT=STATIC_ROOT,
+        INSTALLED_APPS=INSTALLED_APPS,
+        s3static=False,
+    ))
 
 try:
     from local_settings import *
