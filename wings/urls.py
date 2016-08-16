f6f37d660eba90f60bf5918c9ea0303ea669c00e
Anders Pearson <anders@columbia.edu>
2016-08-16 11:11:43 +0100

    django 1.10 style urls.py

--- wings/urls.py
+++ wings/urls.py
@@ -1,86 +1,81 @@
-from django.conf.urls import patterns, include, url
+import django.views.static
+
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.views.generic import RedirectView
+from wings.wings_main.views import (
+    background, first, all_answers, text_answers, all_answers_key,
+    all_answers_key_table, timestamps, summary, launch_participant,
+    exit_materials, participant_exit_materials
+)
+from wings.analytics.views import log_action
+from pagetree.views import move_section
+from wings.main.views import page, edit_page
 
 admin.autodiscover()
 
-urlpatterns = patterns(
-    '',
-
-    ('^$', RedirectView.as_view(url='/admin/wings_main/participant/')),
-    ('^accounts/login/$',
-     RedirectView.as_view(url='/admin/wings_main/participant/')),
-    ('^login/$',
-     RedirectView.as_view(url='/admin/wings_main/participant/')),
-    ('^accounts/profile/$',
-     RedirectView.as_view(url='/admin/wings_main/participant/')),
+urlpatterns = [
+    url('^$', RedirectView.as_view(url='/admin/wings_main/participant/')),
+    url('^accounts/login/$',
+        RedirectView.as_view(url='/admin/wings_main/participant/')),
+    url('^login/$',
+        RedirectView.as_view(url='/admin/wings_main/participant/')),
+    url('^accounts/profile/$',
+        RedirectView.as_view(url='/admin/wings_main/participant/')),
 
 
-    ('^about/$', 'wings.wings_main.views.background',
-     {'content_to_show': 'about'}),
-    ('^help/$', 'wings.wings_main.views.background',
-     {'content_to_show': 'help'}),
-    ('^contact/$', 'wings.wings_main.views.background',
-     {'content_to_show': 'contact'}),
-    ('^credits/$', 'wings.wings_main.views.background',
-     {'content_to_show': 'credits'}),
-    ('^logout/$', RedirectView.as_view(url='/admin/logout/')),
+    url('^about/$', background, {'content_to_show': 'about'}),
+    url('^help/$', background, {'content_to_show': 'help'}),
+    url('^contact/$', background, {'content_to_show': 'contact'}),
+    url('^credits/$', background, {'content_to_show': 'credits'}),
+    url('^logout/$', RedirectView.as_view(url='/admin/logout/')),
 
     # shortcut to first page of the
-    ('^first/$', 'wings.wings_main.views.first'),
+    url('^first/$', first),
 
     # giant table of all answers from all participants
-    ('^all_answers/$', 'wings.wings_main.views.all_answers'),
-    ('^text_answers/$', 'wings.wings_main.views.text_answers'),
-    ('^all_answers_key/$',
-     'wings.wings_main.views.all_answers_key'),
-    ('^all_answers_key_table/$',
-     'wings.wings_main.views.all_answers_key_table'),
-    ('^timestamps/$', 'wings.wings_main.views.timestamps'),
+    url('^all_answers/$', all_answers),
+    url('^text_answers/$', text_answers),
+    url('^all_answers_key/$', all_answers_key),
+    url('^all_answers_key_table/$', all_answers_key_table),
+    url('^timestamps/$', timestamps),
 
     # summary of all pages
-    ('^summary/$', 'wings.wings_main.views.summary'),
+    url('^summary/$', summary),
 
     # start the participant off:
-    (r'^participant/(?P<id_string>\d+)/launch/',
-     'wings.wings_main.views.launch_participant'),
+    url(r'^participant/(?P<id_string>\d+)/launch/', launch_participant),
 
     # exit materials for a particular participant (meant to be accessed by the
     # facilitator from the list of participants)
-    (r'^participant/(?P<id_string>\d+)/exit_materials/',
-     'wings.wings_main.views.exit_materials'),
+    url(r'^participant/(?P<id_string>\d+)/exit_materials/', exit_materials),
 
     # exit materials for the logged in user:
-    (r'^exit_materials/',
-     'wings.wings_main.views.participant_exit_materials'),
+    url(r'^exit_materials/', participant_exit_materials),
 
-    (r'^pagetree/', include('pagetree.urls')),
-    (r'^_quiz/', include('wings.quizblock.urls')),
+    url(r'^pagetree/', include('pagetree.urls')),
+    url(r'^_quiz/', include('wings.quizblock.urls')),
 
     # Logging clicks on UI elements via AJAX:
-    (r'^log_action/(?P<action_id>\d+)/on_section/(?P<section_id>\d+)/$',
-     'wings.analytics.views.log_action'),
+    url(r'^log_action/(?P<action_id>\d+)/on_section/(?P<section_id>\d+)/$',
+        log_action),
 
     # standard django stuff:
-    ('^accounts/', include('djangowind.urls')),
-    (r'^admin/pagetree/section/move/(?P<section_id>\d+)/$',
-     'pagetree.views.move_section', {}, 'move-section'),
-
+    url('^accounts/', include('djangowind.urls')),
+    url(r'^admin/pagetree/section/move/(?P<section_id>\d+)/$', move_section,
+        {}, 'move-section'),
 
     url(r'^admin/', include(admin.site.urls)),
 
-    ('^smoketest/', include('smoketest.urls')),
+    url('^smoketest/', include('smoketest.urls')),
 
-    (r'^uploads/(?P<path>.*)$', 'django.views.static.serve',
-     {'document_root': settings.UPLOADS_ROOT}),
+    url(r'^uploads/(?P<path>.*)$', django.views.static.serve,
+        {'document_root': settings.UPLOADS_ROOT}),
 
     # from forest:
     # Put this after the static files, otherwise it will try
     # to serve them.
-    (r'^edit/(?P<path>.*)$',
-     'wings.main.views.edit_page', {}, 'edit-page'),
-    (r'^(?P<path>.*)$', 'wings.main.views.page'),
-
-
-)
+    url(r'^edit/(?P<path>.*)$', edit_page, {}, 'edit-page'),
+    url(r'^(?P<path>.*)$', page),
+]
